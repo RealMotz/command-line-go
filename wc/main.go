7799492a17bbcdbe1f19373171d5d15114bb2dfc
Carlos Motta <carlos.motta.business@gmail.com>
2024-08-17 18:05:18 +0900

    feat: add suport for -b flag

--- wc/main.go
+++ wc/main.go
@@ -8,12 +8,21 @@ import (
 	"os"
 )
 
-func count(r io.Reader, countLines bool) int {
+func count(r io.Reader, countLines, countBytes bool) int {
+	if countLines && countBytes {
+		fmt.Fprintln(os.Stderr, "invalid flags")
+		return 0
+	}
+
 	// Used to read text from a reader
 	scanner := bufio.NewScanner(r)
 
 	if !countLines {
 		scanner.Split(bufio.ScanWords)
+	}
+
+	if countBytes {
+		scanner.Split(bufio.ScanBytes)
 	}
 
 	// word count
@@ -34,10 +43,11 @@ func count(r io.Reader, countLines bool) int {
 func main() {
 	// Defining a bollean flag -l to count lines instead of words
 	lines := flag.Bool("l", false, "Count lines")
+	bytes := flag.Bool("b", false, "Count bytes")
 	// Parsing the flags provided by the user
 	flag.Parse()
 
 	// Calling the count function to count the number of words
 	// Received from the Standard Input and printing it out
-	fmt.Println(count(os.Stdin, *lines))
+	fmt.Println(count(os.Stdin, *lines, *bytes))
 }
--- wc/main_test.go
+++ wc/main_test.go
@@ -11,7 +11,7 @@ func TestCountWords(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3 word4\n")
 	expected := 4
 
-	actual := count(b, false)
+	actual := count(b, false, false)
 
 	if actual != expected {
 		t.Errorf("Expected %d, got %d instead. \n", expected, actual)
@@ -22,7 +22,18 @@ func TestCountLines(t *testing.T) {
 	b := bytes.NewBufferString("word1 word2 word3\nword4\nword5")
 	expected := 3
 
-	actual := count(b, true)
+	actual := count(b, true, false)
+
+	if actual != expected {
+		t.Errorf("Expected %d, got %d instead. \n", expected, actual)
+	}
+}
+
+func TestCountBytes(t *testing.T) {
+	b := bytes.NewBufferString("0123456789")
+	expected := 10
+
+	actual := count(b, false, true)
 
 	if actual != expected {
 		t.Errorf("Expected %d, got %d instead. \n", expected, actual)
@@ -30,13 +41,33 @@ func TestCountLines(t *testing.T) {
 }
 
 func TestCountWordStdinError(t *testing.T) {
-	overflow := strings.Repeat("a", bufio.MaxScanTokenSize+1)
-	r := strings.NewReader(overflow)
-	expected := 0
+	tests := []struct {
+		name     string
+		input    *strings.Reader
+		lFlag    bool
+		bFlag    bool
+		expected int
+	}{
+		{
+			name:     "buffer max scan token size reached",
+			input:    strings.NewReader(strings.Repeat("a", bufio.MaxScanTokenSize+1)),
+			lFlag:    false,
+			bFlag:    false,
+			expected: 0,
+		},
+		{
+			name:     "-l and -b flags used together",
+			input:    strings.NewReader("test string"),
+			lFlag:    true,
+			bFlag:    true,
+			expected: 0,
+		},
+	}
 
-	actual := count(r, false)
-
-	if actual != expected {
-		t.Errorf("Expected %d, got %d instead. \n", expected, actual)
+	for _, tc := range tests {
+		actual := count(tc.input, tc.lFlag, tc.bFlag)
+		if actual != tc.expected {
+			t.Errorf("%s: Expected %d, got %d instead. \n", tc.name, tc.expected, actual)
+		}
 	}
 }
